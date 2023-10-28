48ac35549e7a611548ca2f1b285ce7d3244c07fc
zschora <yegorssh@gmail.com>
2023-10-29 02:06:15 +0300

    refactor(main): add smart pointers

--- app/main.cpp
+++ app/main.cpp
@@ -9,13 +9,14 @@
 #include <random>
 #include <iomanip>
 #include <algorithm>
+#include <memory>
 
 int main() {
     std::random_device rd;
     std::mt19937 rg(rd());
     std::cout << std::setprecision(3);
 
-    std::vector<Curve*> curves(15);
+    std::vector<std::shared_ptr<Curve>> curves(15);
     for(auto & curve: curves){
         // генерируем слусчайно тип фигуры и параметры
         int type = std::uniform_int_distribution<int>(0, 2)(rg);
@@ -23,13 +24,13 @@ int main() {
         double param2 = std::uniform_real_distribution<double>(0.1, 100)(rg);
         switch (type) {
             case 0:
-                curve = new Circle(param1);
+                curve = std::make_shared<Circle>(param1);
                 break;
             case 1:
-                curve = new Ellipse(param1, param2);
+                curve = std::make_shared<Ellipse>(param1, param2);
                 break;
             case 2:
-                curve = new Helix(param1, param2);
+                curve = std::make_shared<Helix>(param1, param2);
         }
     }
 
@@ -40,14 +41,15 @@ int main() {
     }
 
     // оставим только круги
-    std::vector<Circle*> circles;
-    for(auto& curve : curves){
-        if (auto circle = dynamic_cast<Circle*>(curve))
+    std::vector<std::shared_ptr<Circle>> circles;
+    for(auto& curve: curves){
+        if (auto circle = std::dynamic_pointer_cast<Circle>(curve))
             circles.push_back(circle);
     }
 
     //отсортируем и выведем радиусы
-    std::sort(circles.begin(), circles.end(),[](const Circle* c1,const Circle* c2){
+    std::sort(circles.begin(), circles.end(),
+              [](const std::shared_ptr<Circle>& c1,const std::shared_ptr<Circle>& c2){
         return c1->getRadius() < c2->getRadius();
     });
     for(auto& c: circles) { std::cout << c->getRadius() << ' '; }
@@ -56,7 +58,5 @@ int main() {
     for(auto& c: circles) { total_radius_sum += c->getRadius(); }
     std::cout << "\nTotal radius sum: " << total_radius_sum;
 
-    for(auto& curve : curves)
-        free(curve);
     return 0;
 }
