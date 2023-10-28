a2aade9254968dc1dbe510881147622c91ca836c
zschora <yegorssh@gmail.com>
2023-10-29 00:17:50 +0300

    feature(libcurves): add Helix

--- app/main.cpp
+++ app/main.cpp
@@ -3,19 +3,28 @@
 #include "Vector3D.h"
 #include "Circle.h"
 #include "Ellipse.h"
+#include "Helix.h"
 
 int main() {
     Vector3D v(1, 2, 3);
-    Circle c(1);
     std::cout << "Length: " << v.length();
+
+    Circle c(1);
     std::cout << "\nPoint: ";
     std::cout << c.getPoint(M_PI/4);
     std::cout << "\nDerivative: ";
     std::cout << c.getDerivative(M_PI/4);
+
     Ellipse el(1, 2);
     std::cout << "\nPoint: ";
     std::cout << el.getPoint(M_PI/4);
     std::cout << "\nDerivative: ";
     std::cout << el.getDerivative(M_PI/4);
+
+    Helix h(1, 2);
+    std::cout << "\nPoint: ";
+    std::cout << h.getPoint(M_PI/4);
+    std::cout << "\nDerivative: ";
+    std::cout << h.getDerivative(M_PI/4);
     return 0;
 }
